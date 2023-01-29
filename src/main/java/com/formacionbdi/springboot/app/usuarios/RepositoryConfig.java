76e30e75151f90ae498f14712aa4c3f70127af01
mmgonzalezc <mmgonzalezclara@gmail.com>
2023-01-29 01:10:38 -0600

    Servicio usuarios y roles implementacion de libreria commosn usuarios

--- src/main/java/com/formacionbdi/springboot/app/usuarios/RepositoryConfig.java
+++ src/main/java/com/formacionbdi/springboot/app/usuarios/RepositoryConfig.java
@@ -1,16 +1,20 @@
 package com.formacionbdi.springboot.app.usuarios;
 
+import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Role;
+import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Usuario;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-import com.formacionbdi.springboot.app.usuarios.models.entity.Role;
-import com.formacionbdi.springboot.app.usuarios.models.entity.Usuario;
 
 @Configuration
 public class RepositoryConfig implements RepositoryRestConfigurer{
-
+	/***
+	 * Configuracion para mostrar los ids en el json de respuesta
+	 * @param config
+	 * @param cors
+	 */
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 		config.exposeIdsFor(Usuario.class, Role.class);
--- src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
+++ src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
@@ -1,10 +1,9 @@
 package com.formacionbdi.springboot.app.usuarios.models.dao;
 
+import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Usuario;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
-
-import com.formacionbdi.springboot.app.usuarios.models.entity.Usuario;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
