e944fe70f533b8fe2f63d203af864c60eb508a9f
mmgonzalezc <mmgonzalezclara@gmail.com>
2023-01-28 23:26:22 -0600

    Servicio usuarios y roles

---
+++ src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
@@ -0,0 +1,22 @@
+package com.formacionbdi.springboot.app.usuarios.models.dao;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.formacionbdi.springboot.app.usuarios.models.entity.Usuario;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+/***
+ * Importar crud repository a un endpoint
+ */
+@RepositoryRestResource(path="usuarios")
+public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long>{
+
+	@RestResource(path="buscar-username")
+    Usuario findByUsername(@Param("username") String username);
+	
+	@Query("select u from Usuario u where u.username=?1")
+    Usuario obtenerPorUsername(String username);
+}
