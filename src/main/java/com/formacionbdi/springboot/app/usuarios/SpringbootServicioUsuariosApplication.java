ff17936701cd096f68e4608ea02929af05953542
mmgonzalezc <mmgonzalezclara@gmail.com>
2023-03-03 13:06:28 -0600

    Correccion de import libreria usuario commons

--- src/main/java/com/formacionbdi/springboot/app/usuarios/SpringbootServicioUsuariosApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/usuarios/SpringbootServicioUsuariosApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan({"com.formacionbdi.springboot.app.commons.usuarios.models.entity"})
+@EntityScan({"com.formacionbdi.springboot.app.usuarios.commons.models.entity"})
 public class SpringbootServicioUsuariosApplication {
 
     public static void main(String[] args) {
